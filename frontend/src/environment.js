5dfc9661f5e450183afc895c164d5b3622160755
Sondekar Avdhut <sondekaravdhut25@gmail.com>
2026-09-03 00:02:33 +0530

    frontend deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,3 +1,3 @@
-const isProd=false;
-const server=isProd ? "":"http://localhost:5000";
+const isProd=true;
+const server=isProd ? "https://snip-nuny.onrender.com":"http://localhost:5000";
 export default server;
